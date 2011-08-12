037fa32649ca8da2736afbca9cc5a120b9ea8ff0
Bill St. Clair <billstclair@gmail.com>
2011-08-12 12:16:06 -0400

    Make suggested autoload code less noisy. Add copyright notices.

--- src/autorepo.lisp
+++ src/autorepo.lisp
@@ -44,3 +44,22 @@ subdirectory of DIRECTORY. REPOSITORY-TYPE can be :GIT, :SVN, :DARCS, or :HG"))
 (defmethod download-repo ((repository-type (eql :hg)) url directory)
   (asdf:run-shell-command "cd '~a'; hg clone '~a'"
                           (truename directory) url))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Copyright 2011 Bill St. Clair
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions
+;;; and limitations under the License.
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
